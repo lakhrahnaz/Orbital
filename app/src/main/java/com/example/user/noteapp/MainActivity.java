33f4ae4309b6aaac7724f3f8a475025bdb53ca4f
DESKTOP-5JTN47E\user <lakhrahnaz@gmail.com>
2018-06-29 17:45:53 +0800

    Note list activity added

--- app/src/main/java/com/example/user/noteapp/MainActivity.java
+++ app/src/main/java/com/example/user/noteapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.noteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // called when user taps the notes button
+    public void viewNotes(View view) {
+        Intent intent = new Intent(this, ListNotesActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    // called when user taps the quiz button
 }
