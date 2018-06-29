ca9353772384935857c70afb4c759f0777cf7b22
DESKTOP-5JTN47E\user <lakhrahnaz@gmail.com>
2018-06-29 18:38:32 +0800

    First Push (newnoteactivity)

--- app/src/main/java/com/example/user/noteapp/ListNotesActivity.java
+++ app/src/main/java/com/example/user/noteapp/ListNotesActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.noteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListNotesActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListNotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notes);
+    }
+
+    public void addNotes(View view) {
+        Intent intent = new Intent (this, NewNoteActivity.class);
+        startActivity(intent);
     }
 }
