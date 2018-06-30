090772839ea7393cb0e5d780608e557fe6cc17c8
DESKTOP-5JTN47E\user <lakhrahnaz@gmail.com>
2018-06-30 23:02:08 +0800

    Merge branch 'master' of https://github.com/lakhrahnaz/Orbital?
    Save Function Implemented

---
+++ app/src/main/java/Notes.java
@@ -0,0 +1,21 @@
+public class Notes {
+
+    private String title;
+    private String body;
+
+    public Notes() {
+    }
+
+    public Notes(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
--- app/src/main/java/com/example/user/noteapp/NewNoteActivity.java
+++ app/src/main/java/com/example/user/noteapp/NewNoteActivity.java
@@ -2,13 +2,38 @@ package com.example.user.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
 
 public class NewNoteActivity extends AppCompatActivity {
+
+    EditText Name;
+    EditText Body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
     }
-    //placeholder
+    public void onSaveClick (View view){
+        Body = (EditText) findViewById(R.id.editText);
+        Name = (EditText) findViewById(R.id.editText2);
+        Save(Name);
+
+    }
+    public void Save (EditText title) {
+        try {
+            OutputStreamWriter output = new OutputStreamWriter(openFileOutput(title.getText().toString(), 0));
+            output.write(Body.getText().toString());
+            output.close();
+            Toast.makeText(this, "Save Complete", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Error: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
